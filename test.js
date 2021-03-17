659c4e4761e478fb93bc6297890d9a7e6073e78e
Rick Bunte <rickbunte2000@gmail.com>
2021-03-17 12:46:48 +0100

    Experimented with types
    I did some minor experimenting

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-"use strict";
 var message = 'Hello World';
-console.log(message);
+var p1 = { name: "Bill", age: 16 };
+var p2 = { name: "Owen", age: 24 };
+var p3 = { name: "Rick", age: 21 };
+var getName = function (person) {
+    if (typeof (person.name) === "string")
+        return person.name;
+    else
+        return "Invalid name!";
+};
+console.log(getName(p1));
+console.log(getName(p2));
+console.log(getName(p3));
--- test.ts
+++ test.ts
@@ -1,2 +1,23 @@
+type Person<a> =
+{
+    name: a,
+    age: number
+}
+
 let message: string = 'Hello World';
-console.log(message);
+
+let p1 : Person<string> = {name: "Bill", age: 16}
+let p2 : Person<string> = {name: "Owen", age: 24}
+let p3 : Person<string> = {name: "Rick", age: 21}
+
+let getName =
+    function(person : Person<string>) : string {
+        if(typeof(person.name) === "string")
+            return person.name
+        else
+            return "Invalid name!"
+    }
+
+console.log(getName(p1));
+console.log(getName(p2));
+console.log(getName(p3));
